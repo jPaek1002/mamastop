f9354e8625def15820274e0b1a97e16f8aecccb2
Jay Paek <46653581+jPaek1002@users.noreply.github.com>
2022-04-22 13:54:51 -0700

    implement easy ocr and add test dataset

---
+++ main.py
@@ -0,0 +1,6 @@
+from mamastop import MaMaStop
+
+if __name__ == "__main__":
+    tl = MaMaStop()
+    result = tl.img_to_string('80/3.jpg')
+    print(result)
--- mamastop.py
+++ mamastop.py
@@ -1,12 +1,34 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import os
-from bs4 import BeautifulSoup as bs
+from os import listdir
+import cv2
+from PIL import Image
+import easyocr as ocr
+import ssl
+import konlpy
 
-if os.getcwd() not in os.environ["PATH"]:
-    os.environ["PATH"] += os.pathsep + os.getcwd()
+class MaMaStop:
+    def __init__(self):
+        self.dirpath = ""
+        self.nouns = []
+        reader = ocr.Reader(['ko', 'en'])
+        ssl._create_default_https_context = ssl._create_unverified_context
 
-driver = webdriver.Firefox()
-driver.get("https://papago.naver.com/?sk=ko&tk=en&st=바보")
-content = driver.page_source()
-source = bs(content)
+    def process_dir(self, dirpath):
+        self.dirpath = os.path.abspath(dirpath)
+        fnames = [os.path.join(os.path.abspath(self.dirpath), x) for x in listdir(self.dirpath)]
+        for fname in fnames:
+            self.img_to_string(fname)
+
+    def img_to_string(self, filepath):
+        reader = ocr.Reader(['ko', 'en'])
+        return "".join(reader.readtext(filepath, detail = 0))
+
+    def find_tl(self):
+        if os.getcwd() not in os.environ["PATH"]:
+            os.environ["PATH"] += os.pathsep + os.getcwd()
+
+        driver = webdriver.Firefox()
+        driver.get("https://papago.naver.com/?sk=ko&tk=en&st=바보")
+
